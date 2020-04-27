d04513166bb40a4ea1addf195a9cd2807ba3c785
AHFC <hf.cunha@yahoo.com>
2020-04-27 19:28:30 -0300

    limpando branch e fazendo push com o .gitignore ativado

--- Debug/timestamp__genmod.f90
+++
@@ -1,9 +0,0 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Fri Apr 24 15:49:47 2020
-        ! This source file is for reference only and may not completely
-        ! represent the generated interface used by the compiler.
-        MODULE TIMESTAMP__genmod
-          INTERFACE 
-            SUBROUTINE TIMESTAMP
-            END SUBROUTINE TIMESTAMP
-          END INTERFACE 
-        END MODULE TIMESTAMP__genmod
